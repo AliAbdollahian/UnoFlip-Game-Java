266cb69910ae8bb768bbb96bd7c0cf7bedcf300c
warda <73768064+1ws10@users.noreply.github.com>
2023-10-17 16:23:27 -0400

    Update Deck.java

--- src/Deck.java
+++ src/Deck.java
@@ -51,11 +51,19 @@ public class Deck {
                 deck.add(new Card(Color.yellow, Color.purple, Value.values()[j], Value.values()[j]));
             }
 
-            for (int m=1; m==12; m++){
-                deck.add(new Card(Color.blue, Color.pink, Value.values()[j], Value.values()[j]));//, Color.pink, Value.values([j]));
-                deck.add(new Card(Color.green,  Color.teal, Value.values()[j], Value.values()[j]));
-                deck.add(new Card(Color.red, Color.orange, Value.values()[j], Value.values()[j]));
-                deck.add(new Card(Color.yellow, Color.purple, Value.values()[j], Value.values()[j]));
+            for (int m=1; m==4; m++){
+                deck.add(new Card(Color.special, Color.special, Value.WILD, Value.WILD));
+                deck.add(new Card(Color.special,  Color.special, Value.WILD_DRAW_2, Value.WILD_DRAW_COLOR));
+                //deck.add(new Card(Color.red, Color.orange, Value.values()[j], Value.values()[j]));
+                //deck.add(new Card(Color.yellow, Color.purple, Value.values()[j], Value.values()[j]));
+            }
+
+            // DRAW_1, DRAW_5, REVERSE, SKIP, SKIP_EVERYONE, FLIP,
+            for (int m=1; m==8; m++) {
+                deck.add(new Card(Color.special, Color.special, Value.DRAW_1, Value.DRAW_5));
+                deck.add(new Card(Color.special, Color.special, Value.SKIP, Value.SKIP_EVERYONE));
+                //deck.add(new Card(Color.special, Color.special, Value.FLIP, Value.REVERSE));
+                //deck.add(new Card(Color.yellow, Color.purple, Value.values()[j], Value.values()[j]));
             }
         }
 
