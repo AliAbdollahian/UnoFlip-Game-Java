667395edb88badaa9fa4a537efba932c1fa37080
Cinacho23 <71153062+Cinacho23@users.noreply.github.com>
2023-11-21 19:32:47 -0500

    Create Card.java

---
+++ src/Card.java
@@ -0,0 +1,49 @@
+/**
+ * This is the Card class that defines which cards in the deck will
+ * be used in the UNOFlip game.
+ */
+
+public class Card {
+    private final Color lightBorder;
+    private final Value lightNum;
+
+    /**
+     * Constructor for Card class
+     * @param lightBorder the colors on the light side
+     * @param lightNum the value on the light side
+     */
+    public Card(Color lightBorder, Value lightNum){
+        this.lightBorder = lightBorder;
+        this.lightNum = lightNum;
+
+    }
+
+    /**
+     * getter method for the color of the light side of the cards.
+     * @return Color the color on the light side of the card.
+     */
+    public Color getLightBorder(){
+        return this.lightBorder;
+    }
+
+
+    /**
+     * getter method for the value of the light side of the cards.
+     * @return Valye the value on the light side of the card.
+     */
+    public Value getLightNum(){
+        return this.lightNum;
+    }
+
+
+    /**
+     * Returns the color and value description of the card played.
+     * @return      String
+     */
+    public String toString(){
+        
+        String str =  this.lightBorder + ": " + this.lightNum;;
+        
+        return str;
+    }
+}
