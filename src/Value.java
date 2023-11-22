616d310c1f178c9be8fab77f5182ee74e7cce8da
Cinacho23 <71153062+Cinacho23@users.noreply.github.com>
2023-11-21 19:33:20 -0500

    Create Value.java

---
+++ src/Value.java
@@ -0,0 +1,12 @@
+/**
+ * This is the Value class for the deck
+ */
+
+public enum Value {
+    ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE,
+    DRAW_1, DRAW_5, REVERSE, SKIP, SKIP_EVERYONE, FLIP, WILD,WILD_DRAW_2, WILD_DRAW_COLOR;
+
+    private Value(){
+
+    }
+}
